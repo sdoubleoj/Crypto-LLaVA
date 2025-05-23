0617648def920a0528e70a5a5a6bf2037bfd1931
SuJeong <78069770+sdoubleoj@users.noreply.github.com>
2025-05-23 09:32:53 +0900

    Update finetune_siglip.sh

--- scripts/train/finetune_siglip.sh
+++ scripts/train/finetune_siglip.sh
@@ -1,31 +1,44 @@
-export OMP_NUM_THREADS=8
-export NCCL_IB_DISABLE=0
-export NCCL_IB_GID_INDEX=3
-export NCCL_SOCKET_IFNAME=eth0
-export NCCL_DEBUG=INFO
+#!/bin/bash
 
-LLM_VERSION="Qwen/Qwen2-7B-Instruct"
+export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32
+
+export OMP_NUM_THREADS=8
+export CUDA_VISIBLE_DEVICES=5
+export NCCL_DEBUG=INFO 
+
+export NCCL_ASYNC_ERROR_HANDLING=1
+export NCCL_BLOCKING_WAIT=1
+export NCCL_IB_DISABLE=1
+export TORCH_NCCL_BLOCKING_WAIT=1
+export NCCL_TIMEOUT=600
+
+LLM_VERSION="Qwen/Qwen2.5-1.5B"
 LLM_VERSION_CLEAN="${LLM_VERSION//\//_}"
-VISION_MODEL_VERSION="google/siglip-so400m-patch14-384"
+VISION_MODEL_VERSION="google/siglip2-so400m-patch14-384"
 VISION_MODEL_VERSION_CLEAN="${VISION_MODEL_VERSION//\//_}"
 
-############### Pretrain ################
+MY_DATA_PATH="data/instruction/train/merged_train_v0.2.json"
+MY_IMAGE_FOLDER="Crypto-LLaVA/"
+MID_RUN_NAME="finetune-${VISION_MODEL_VERSION_CLEAN}-${LLM_VERSION_CLEAN}"
+OUTPUT_DIR="Crypto-LLaVA/checkpoints/${MID_RUN_NAME}"
+
+echo "RUN NAME: ${MID_RUN_NAME}"
 
 PROMPT_VERSION="qwen_1_5"
 
-BASE_RUN_NAME="llavanext-${VISION_MODEL_VERSION_CLEAN}-${LLM_VERSION_CLEAN}-mlp2x_gelu-pretrain_blip558k_plain"
+BASE_RUN_NAME="cryptollava-${VISION_MODEL_VERSION_CLEAN}-${LLM_VERSION_CLEAN}-mlp2x_gelu"
 echo "BASE_RUN_NAME: ${BASE_RUN_NAME}"
 
-CKPT_PATH=$LLM_VERSION # this could also be the previous stage checkpoint
+CKPT_PATH=$LLM_VERSION
 
-ACCELERATE_CPU_AFFINITY=1 torchrun --nproc_per_node="${NUM_GPUS}" --nnodes="${NNODES}" --node_rank="${RANK}" --master_addr="${ADDR}" --master_port="${PORT}" \
+torchrun --nproc_per_node=1 \
     llava/train/train_mem.py \
+    --lora_enable True \
     --deepspeed scripts/zero3.json \
     --model_name_or_path ${CKPT_PATH} \
     --version ${PROMPT_VERSION} \
-    --data_path=llava_1_6.json \
-    --image_folder your_image_folder \
-    --pretrain_mm_mlp_adapter="/checkpoints/projectors/${BASE_RUN_NAME}/mm_projector.bin" \
+    --data_path ${MY_DATA_PATH} \
+    --image_folder ${MY_IMAGE_FOLDER} \
     --mm_tunable_parts="mm_vision_tower,mm_mlp_adapter,mm_language_model" \
     --mm_vision_tower_lr=2e-6 \
     --vision_tower ${VISION_MODEL_VERSION} \
@@ -35,18 +48,18 @@ ACCELERATE_CPU_AFFINITY=1 torchrun --nproc_per_node="${NUM_GPUS}" --nnodes="${NN
     --mm_use_im_patch_token False \
     --group_by_modality_length True \
     --image_aspect_ratio anyres \
-    --image_grid_pinpoints "[(384, 768), (768, 384), (768, 768), (1152, 384), (384, 1152)]" \
+    --image_grid_pinpoints "[(384, 384)]" \
     --mm_patch_merge_type spatial_unpad \
     --bf16 True \
     --run_name $MID_RUN_NAME \
-    --output_dir "/checkpoints/${MID_RUN_NAME}" \
+    --output_dir ${OUTPUT_DIR} \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 4 \
+    --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 3000 \
+    --save_steps 1000 \
     --save_total_limit 1 \
     --learning_rate 1e-5 \
     --weight_decay 0. \
@@ -54,7 +67,7 @@ ACCELERATE_CPU_AFFINITY=1 torchrun --nproc_per_node="${NUM_GPUS}" --nnodes="${NN
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 True \
-    --model_max_length 32768 \
+    --model_max_length 2048 \
     --gradient_checkpointing True \
     --dataloader_num_workers 16 \
     --lazy_preprocess True \
@@ -63,5 +76,3 @@ ACCELERATE_CPU_AFFINITY=1 torchrun --nproc_per_node="${NUM_GPUS}" --nnodes="${NN
     --torch_compile_backend "inductor" \
     --dataloader_drop_last True \
     --attn_implementation sdpa
-
-# You can delete the sdpa attn_implementation if you want to use flash attn
